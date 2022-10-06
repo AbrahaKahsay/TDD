9e860930ea5e3502a37c3eb434fc2e57bc10ed1d
AbrahaKahsay <abrahak81@gmail.com>
2022-10-06 22:26:43 +0300

    Create tests for fizbuzz method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -24,5 +24,18 @@ describe  Solver do
         end
     end
 
-
+    context 'check the third method (fizzBuzz)' do 
+        it 'when input is divided by 3 and 5 it will return fizzbuzz' do 
+            expect(solver.fizzbuzz(30)).to eq("fizzbuzz") 
+        end 
+        it 'when the number is divded by 3 it will return fizz' do 
+            expect(solver.fizzbuzz(3)).to eq("fizz")
+         end 
+         it 'when the number is divded by 5 it will return buzz' do 
+            expect(solver.fizzbuzz(5)).to eq("buzz") 
+        end 
+        it 'when its not fizz or buzz or fizzbuzz return string number' do
+             expect(solver.fizzbuzz(7)).to eq("7") 
+            end 
+        end 
 end
